9fff73296597c494598e729d60bbe9286a574a96
ziqing97 <ziqing.yu0623@gmail.com>
2020-09-16 01:29:00 +0200

    update ET

---
+++ My Job/Code/Sandbox.m
@@ -0,0 +1,19 @@
+clear all
+close all
+clc
+
+
+load("ERA5_R_OB.mat")
+load("HTESSEL_R_OB.mat")
+load("LISFLOOD_R_OB.mat")
+load("Ob_RU_0000081_mon.mat")
+load("ORCHIDEE_R_OB.mat")
+load("PCRGLOBWB_R_OB.mat")
+load("R_GLDAS_CLSM_OB.mat")
+load("R_GLDAS_NOAH_OB.mat")
+load("R_GLDAS_VIC_OB.mat")
+load("SURFEX_TRIP_R_OB.mat")
+load("W3RA_R_OB.mat")
+load("WaterGAP3_R_OB.mat")
+
+load("R_insitu_OB.mat")
